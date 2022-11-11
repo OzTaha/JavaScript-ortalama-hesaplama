9e7079011cbd58dcf8cf1c80e1720869ece647de
berkbaski <berk.baski@innovance.com.tr>
2022-11-11 14:41:07 +0300

    Update code base and add form

--- js/calculate.js
+++ js/calculate.js
@@ -31,10 +31,13 @@ const GRADE_VALUES = [
   },
 ];
 
-function calculate() {
-  const midterm = Number(document.getElementById("vize").value);
-  const final = Number(document.getElementById("final").value);
-
+/**
+ * Calculate the grade of the student
+ * @param {Number} midterm Midterm grade
+ * @param {Number} final Final grade
+ * @returns {String} Grade of the student
+ */
+function calculate(midterm, final) {
   if (midterm < 0 || midterm > 100 || final < 0 || final > 100) {
     alert("Lütfen 0 ile 100 arasında bir değer giriniz.");
     return;
@@ -45,10 +48,23 @@ function calculate() {
     (grade) => average >= grade.min && average <= grade.max
   );
 
-  document.getElementById(
-    "sonuc"
-  ).innerHTML = `Ortalama : ${average} = ${grade.grade} - ${grade.message}`;
+  return `Ortalama : ${average} = ${grade.grade} - ${grade.message}`;
 }
 
-const calculateButton = document.querySelector("#hesapla");
-calculateButton.addEventListener("click", calculate);
+/**
+ * Display the grade of the student
+ * @param {String} result Result of the calculation
+ */
+function showResult(result) {
+  document.getElementById("result").innerHTML = result;
+}
+
+document.querySelector("#calculateForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const midterm = e.target.elements.midterm.value;
+  const final = e.target.elements.final.value;
+
+  const result = calculate(midterm, final);
+  showResult(result);
+});
